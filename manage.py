942551689086090c9c118e31dc8800f1f5c4278b
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-18 17:14:39 +0300

    create model class,connects to databse and configured the blueprint for the auth folder

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager, Server
+from app.models import User,OAuth,Follow
+from  flask_migrate import Migrate, MigrateCommand
 
 # instances for the create_app
 app = create_app('development')
@@ -9,9 +11,12 @@ manager = Manager(app)
 manager.add_command('server', Server)
 
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 @manager.shell
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, User=User,Follow=Follow,OAuth = OAuth)
 
 
 if __name__ == '__main__':
