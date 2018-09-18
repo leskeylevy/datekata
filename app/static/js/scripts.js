c4f667184b5d2ac8900e0ddeb7d08ab372dd5251
Nickghost inc <you@example.com>
2018-09-18 14:33:51 +0300

    navbar style added

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,13 +1,14 @@
-$("#toggle").click( function(event){
-    event.preventDefault();
-    if ($(this).hasClass("isDown") ) {
-        $( ".navbar-fixed-top" ).animate({ "margin-top": "-62px" }, "fast" );  
-        $( "#content" ).animate({ "margin-top": "5px" }, "fast" );
-        $(this).removeClass("isDown");
-    } else {
-        $( ".navbar-fixed-top" ).animate({ "margin-top": "0px" }, "fast" );
-        $( "#content" ).animate({ "margin-top": "80px" }, "fast" );
-        $(this).addClass("isDown");
-    }
-    return false;
-});
+// Setting up the Variables
+var bars = document.getElementById("nav-action");
+var nav = document.getElementById("nav");
+
+
+//setting up the listener
+bars.addEventListener("click", barClicked, false);
+
+
+//setting up the clicked Effect
+function barClicked() {
+  bars.classList.toggle('active');
+  nav.classList.toggle('visible');
+}
