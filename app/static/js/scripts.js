8772348256a8319e813359ed33ed2ff65b251a7d
Nickghost inc <you@example.com>
2018-09-18 15:34:17 +0300

    500 INTERNAL SEVER ERROR PAGE modified

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -58,3 +58,78 @@ $(window).on('mousemove click', function(e) {
 });
 
 animate();
+
+
+
+=======================================
+
+/*## 500 INTERNAL SERVER ERROR PAGE SECTION ##*/
+
+======================================= 
+
+
+Object.assign(document.body.style, {
+  color: "#e7852d",
+  background: "linear-gradient(to right,#404142,#212326)"
+});
+
+const h1 = document.createElement("h1");
+
+Object.assign(h1.style, {
+  background: "linear-gradient(to right,#e7852d,#dc124e)",
+  "-webkit-background-clip": "text",
+  "-webkit-text-fill-color": "transparent",
+  fontFamily: "Arial,Helvetica,sans-serif",
+  textAlign: "center",
+  width: "80%",
+  margin: "auto"
+});
+
+h1.innerHTML = 'How can a "500 &mdash; Internal Server Error" error happen?';
+
+document.body.appendChild(h1);
+
+setInterval(() => {
+  const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+  const fontFamilies = [
+    "Arial Black,Gadget,sans-serif",
+    "Impact,Charcoal,sans-serif",
+    "Verdana,Geneva,sans-serif",
+    "Lucida Console,Monaco,monospace"
+  ];
+  const fontWeights = ["normal", "bold"];
+  const fontStyles = ["normal", "italic", "oblique"];
+  const operations = [
+    () => {
+      const lhs = rand(1, 500);
+      const rhs = 500 - lhs;
+      return lhs + " + " + rhs + " = " + (lhs + rhs);
+    },
+    () => {
+      const lhs = rand(1, 100);
+      const rhs = 500 / lhs;
+      return lhs + " * " + rhs + " = " + Math.round(lhs * rhs);
+    }
+  ];
+
+  const p = document.createElement("p");
+
+  Object.assign(p.style, {
+    position: "absolute",
+    top: rand(0, document.documentElement.clientHeight) + "px",
+    left: rand(0, document.documentElement.clientWidth) + "px",
+    fontSize: rand(1, 5) + "em",
+    fontFamily: fontFamilies[rand(0, 3)],
+    fontWeight: fontWeights[rand(0, 1)],
+    fontStyle: fontStyles[rand(0, 2)],
+    transform: "rotate(" + rand(-25, 25) + "deg)"
+  });
+
+  p.innerHTML = operations[rand(0, 1)]();
+
+  document.body.appendChild(p);
+
+  if (document.body.childNodes.length > 15) {
+    document.body.removeChild(document.querySelectorAll("p")[0]);
+  }
+}, 1000);
